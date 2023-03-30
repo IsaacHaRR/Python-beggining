a8324e27638d3df37fdd98652163df0a57f086c4
IsaacHaRR <isaac15rosendo@gmail.com>
2023-03-30 12:59:16 -0300

    open_files

---
+++ files.py
@@ -0,0 +1,12 @@
+
+# r -> to read the file
+# a -> to put more information
+# r+ -> to read and put information
+
+file = open("file.txt","r")
+
+print(file.read())
+
+# close the file
+file.close()
+
